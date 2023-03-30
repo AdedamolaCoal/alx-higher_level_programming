f4d33474886e90e73d69948e10611c1758788f2a
AdedamolaC <adedamolacoal@outlook.com>
2023-03-30 19:47:44 +0100

    Update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 
-for alphabet in range(ord('a'), ord('z')+1):
-    if alphabet == 'e' or alphabet == 'q':
-        continue
-    print("{:c}".format(alphabet), end="")
+"".join([letter for letter in string.ascii_lowercase if letter not in "qe"])
